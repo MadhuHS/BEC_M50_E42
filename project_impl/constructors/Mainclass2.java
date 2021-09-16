493971fcc2bfe23e81ce02ec08424c7a1f5be975
Madhuhs <madhuhs943@gmail.com>
2021-09-16 09:50:48 +0530

    Adding signup logic to create User DTO and hard coding UID

--- project_impl/constructors/Mainclass2.java
+++ project_impl/constructors/Mainclass2.java
@@ -8,14 +8,22 @@ class User
   long mob;
   String email;
   String pwd;
+
+
 }
 
 //Service - Biz Logics
 class UserService
 {
+   User userDto;
    public void signup(String name,long mob,String email,String pwd)
    {
      
+     //create DTO
+     userDto = new User(1234,name,mob,email,pwd);
+     
+     //To-Do : save DTO data to DB
+     System.out.println();
    }
 
    public void viewProfile()

a9d432e61d6a424d4f8d0fcef32bcf41b4b424d5
Madhuhs <madhuhs943@gmail.com>
2021-09-16 09:55:18 +0530

    Adding Random class to generate random UID

--- project_impl/constructors/Mainclass2.java
+++ project_impl/constructors/Mainclass2.java
@@ -18,7 +18,15 @@ class UserService
    User userDto;
    public void signup(String name,long mob,String email,String pwd)
    {
+     //Generate random integer for UID
+     Random r1 = new Random();
+     int uid = r1.nextInt(10000);//inBuilt-method
      
+     //create DTO
+     userDto = new User(uid,name,mob,email,pwd);
+     
+     //To-Do : save DTO data to DB
+     System.out.println();
      //create DTO
      userDto = new User(1234,name,mob,email,pwd);
      

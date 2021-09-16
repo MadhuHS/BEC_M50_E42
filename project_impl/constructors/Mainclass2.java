627841a81a6a6b041e4f61279428959a8ee32b79
Madhuhs <madhuhs943@gmail.com>
2021-09-16 09:46:19 +0530

    Initial structure of Project without any Logic - Project Step : 1

---
+++ project_impl/constructors/Mainclass2.java
@@ -0,0 +1,37 @@
+import java.util.Random;//inbuilt class
+
+//Entity - DTO
+class User
+{
+  int uid;
+  String name;
+  long mob;
+  String email;
+  String pwd;
+}
+
+//Service - Biz Logics
+class UserService
+{
+   public void signup(String name,long mob,String email,String pwd)
+   {
+     
+   }
+
+   public void viewProfile()
+   {
+     
+   }
+}
+
+//Front-End
+class Mainclass
+{
+	public static void main(String[] args)
+	{
+	   System.out.println("Program starts..");
+       
+    
+	   System.out.println("Program ends..");	
+	}
+}
